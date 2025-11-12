79ad3a3fced89f8e21d690b9505fd49aa3bb4bed
hunterjohnstone <hunterjohnst1@gmail.com>
2025-11-12 15:19:31 +0100

    vercel insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -72,6 +73,7 @@ export default function RootLayout({
         data-lang={lang}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   )
